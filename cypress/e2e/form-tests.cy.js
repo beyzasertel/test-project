5456e64a35bdcd9a870f1f2629840ab0027bc20c
beyzasertel <beyza786@outlook.com>
2025-10-04 22:44:38 +0300

    test for email added

--- cypress/e2e/form-tests.cy.js
+++ cypress/e2e/form-tests.cy.js
@@ -1,6 +1,9 @@
 it("Email yanlış girdim", () => {
-  cy.get('input[type="email"]').type("Beyza");
-  cy.get('button[type="submit"]').click();
+  cy.visit("http://localhost:5173");
+  cy.get('[data-cy="email-input"]').type("Beyza");
+  cy.get('[data-cy="password-input"]').type("deneme123");
+  cy.get('[data-cy="terms-checkbox"]').check();
+  cy.get('[data-cy="submit-btn"]').click();
   cy.on("window:alert", (text) => {
     expect(text).to.contains("Lütfen geçerli bir e-posta adresi girin!");
   });
